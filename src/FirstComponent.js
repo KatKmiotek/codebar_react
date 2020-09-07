eeb4afd1e069fc5363cf834b83b37a57724a2f5f
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2020-09-07 19:18:15 +0100

    :tada:

---
+++ src/FirstComponent.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+// class FirstComponent extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             state: ''
+//         }
+//     }
+//     render() {
+//         return (
+//             <p>Hello Kat</p>
+//         )
+//     }
+// }
+
+const FirstComponent = () => {
+    return (
+        <p>Hello Kat</p>
+    )
+}
+export default FirstComponent
